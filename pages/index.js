73442730ea8893717e9fce3e0d3135265da811e7
Djoa1 <angelon.jc@outlook.com>
2022-01-25 17:48:08 -0300

    segunda aula 01

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,9 @@
+
 import { Box, Button, Text, TextField, Image } from '../node_modules/@skynexui/components';
+import React from 'react';
+import { useRouter } from 'next/router';
 import appConfig from '../pages/config.json';
 //teste
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-    `}</style>
-  );
-}
 
 function Title(props) {
   const Tag = props.tag || 'h1';
@@ -46,25 +21,15 @@ function Title(props) {
   );
 }
 
-// Componente React
-// function HomePage() {
-//     // JSX
-//     return (
-//         <div>
-//             <GlobalStyle />
-//             <Titulo tag="h2">Boas vindas de volta!</Titulo>
-//             <h2>Discord - Alura Matrix</h2>
-//         </div>
-//     )
-// }
-// export default HomePage
+
 
 export default function PaginaInicial() {
-  const username = 'peas';
+  //const username = 'peas';
+  const [username, setUsername] = React.useState('djoa1');
+  const roteamento = useRouter();
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -91,6 +56,15 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              console.log('Alguém submeteu o form');
+
+              roteamento.push('/chat');
+              //jeito antigo
+              //window.location.href = '/chat';
+
+            }}
             styleSheet={{
               display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
               width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -100,8 +74,16 @@ export default function PaginaInicial() {
             <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
               {appConfig.name}
             </Text>
-
-            <TextField
+            
+            <TextField                  
+            value={username}
+                  onChange ={function (event){
+                    console.log("usario digitou", event.target.value);
+                    //onde ta o valor:
+                    const valor = event.target.value;
+                    //trocar o valor da variavel
+                    setUsername(valor);
+                  }}
               fullWidth
               textFieldColors={{
                 neutral: {
